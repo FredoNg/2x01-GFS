959044d6ecaa6540e13509ece0957aad9e69f357
kh.ang <57080884+koonhwa1691@users.noreply.github.com>
2020-11-26 17:21:34 +0800

    Add files via upload

---
+++ project/lectPage.php
@@ -0,0 +1,46 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+?>
+<html lang="en">
+
+    <head>
+        <title>Creole | (Shop)</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="Creole | Bringing People Together Through Fashion">
+        <meta name="keywords" content="Fasion, Clothes, Dress, Tops, Bottoms">
+        <link rel="stylesheet" href="sideNav.css">
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+        <script src="js/filter.js"></script> 
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="stylesheet" href="css/shopfilter.css" />
+        
+    </head>
+    <body> 
+         <main>
+
+            <!-- Navigation  -->
+            <?php
+            include 'nav.inc.php';
+            ?>
+            <!--Navigation End  -->
+            <!-- side nav bar -->
+            <?php
+            include 'sidenavbar.php';
+            ?>
+                        <br>
+            <?php
+            //footer
+            ?>
+            <!--Footer End-->
+        </main>
+    </body>
+</html>
