d4c7b65560eae3d90c6fb79f2f647946be74d104
yolothedino <darhdevil@hotmail.com>
2020-11-27 17:31:07 +0800

    cleaned up login page. textbox not working, help please

--- project/login.php
+++ project/login.php
@@ -7,15 +7,11 @@ require_once('../protected/config.php');
     define('DB_PASSWORD', 'JLC4662091');
     define('DB_DATABASE', 'sql1902664clj');
     $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
-   
-    $sql = "SELECT * FROM Users";
-    $result = mysqli_query($db,$sql);
-    
 
 ?>
 <html lang="en">
     <head>
-        <title>Creole | (Login)</title>
+        <title>G.F.S | (Login)</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="description" content="Creole | Bringing People Together Through Fashion">
@@ -33,35 +29,27 @@ require_once('../protected/config.php');
     </head>
 
     <body>
-        <?php
-        include 'nav.inc.php';
-        ?>
         <main>
-            <br>
-            <br>
             <div class="container-fluid">
-                <h1>
-                    <?php while ($row = mysqli_fetch_array($result)) 
-                    {
-                    echo $row['uEmail'];
-                    }?>"</h1>
-    
-                <h1>Member Login</h1>
+                <h1>G.F.S Login</h1>
                 <hr class="us">
-                <p>For new members, please click on Sign Up to create a new account!</p>
+                <p>Please log in using your given email and password.</p>
+                
                 <hr class="us">
-                <form name="myLogin" id="myLogin" action="process_login.php" onsubmit="return myLogin()" method="post">
-                    Email: <input type="email" pattern="[^@]+@[^@]+\.[a-zA-Z]{2,}" class="form-control" name="email" id="email" placeholder="Enter Email" required aria-label="email login"><br>
-                    <hr class="us">
-                    Password: <input type="password" class="form-control" name="password" id="password" placeholder="Enter Password" required aria-label="Enter password"><br>
-                    <hr class="us">
-                    <td>result</td>
-                    <button type="submit" class="btn btn-default">Submit</button>
-                </form>
+                <div class="col-xs-6  col-md-3 margin-top-bot" >
+                    <h3>Student/Lecturer Login</h3>
+                        <form style="border:1px; ">
+                            Enter Email:
+                            <input type="text" style="margin-left: 0px; border:1px solid #ccc; " pattern="[^@]+@[^@]+\.[a-zA-Z]{2,}" class="form-control" name="email" id="email" placeholder="Enter Email" required aria-label="email login"><br>
+                            Enter Password:
+                            <input type="password" class="form-control" name="password" id="password" placeholder="Enter Password" required aria-label="Enter password"><br>
+                            <button type="submit" class="btn btn-default">Log In</button>
+                        </form>
+ 
+                </div>
 
             </div>
         </main>
-        <?php include "footer.inc.php" ?>
     </body>
 
 </html>
